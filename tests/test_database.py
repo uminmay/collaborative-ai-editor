6bd6ca8eda40844885857eb9666f6e57f62e3a92
Minmay Upadhyay <uminmay@gmail.com>
2025-01-31 18:58:21 +0000

    db testing fixes #2

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,48 +1,53 @@
-# tests/test_database.py
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.db.database import Base
-from app.db import crud, models
-from app.main import app
-from fastapi.testclient import TestClient
+from app.db import models
 import os
 
 SQLALCHEMY_TEST_DATABASE_URL = "sqlite:///./test.db"
 
-@pytest.fixture(scope="function")
-def test_db():
-    # Create test database
+@pytest.fixture(scope="session")
+def test_engine():
     engine = create_engine(SQLALCHEMY_TEST_DATABASE_URL)
-    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    
-    # Create tables
-    Base.metadata.create_all(bind=engine)
-    
-    # Create test session
+    models.Base.metadata.create_all(bind=engine)
+    yield engine
+    models.Base.metadata.drop_all(bind=engine)
+    if os.path.exists("./test.db"):
+        os.remove("./test.db")
+
+@pytest.fixture(scope="function")
+def test_db(test_engine):
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
     db = TestingSessionLocal()
     try:
         yield db
     finally:
+        db.rollback()
         db.close()
-        Base.metadata.drop_all(bind=engine)
-        if os.path.exists("./test.db"):
-            os.remove("./test.db")
 
 def test_create_project(test_db):
-    project = crud.create_project(test_db, name="test_project", path="/test_project")
+    project = models.Project(name="test_project", path="/test_project")
+    test_db.add(project)
+    test_db.commit()
+    assert project.id is not None
     assert project.name == "test_project"
-    assert project.path == "/test_project"
 
 def test_get_project(test_db):
-    project = crud.create_project(test_db, name="test_project", path="/test_project")
-    fetched_project = crud.get_project(test_db, path="/test_project")
-    assert fetched_project.name == project.name
-    assert fetched_project.project_id == project.project_id
+    project = models.Project(name="test_project", path="/test_project")
+    test_db.add(project)
+    test_db.commit()
+    
+    saved_project = test_db.query(models.Project).filter_by(path="/test_project").first()
+    assert saved_project is not None
+    assert saved_project.name == project.name
 
 def test_delete_project(test_db):
-    project = crud.create_project(test_db, name="test_project", path="/test_project")
-    deleted_project = crud.delete_project(test_db, path="/test_project")
-    assert deleted_project.id == project.id
-    fetched_project = crud.get_project(test_db, path="/test_project")
-    assert fetched_project is None
+    project = models.Project(name="test_project", path="/test_project")
+    test_db.add(project)
+    test_db.commit()
+    
+    test_db.delete(project)
+    test_db.commit()
+    
+    saved_project = test_db.query(models.Project).filter_by(path="/test_project").first()
+    assert saved_project is None
